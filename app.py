0dd598df7daabe3dd9551457288cb44eeb32fc55
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-18 23:01:49 +0900

    feat: momentoの使用

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import re
 import time
+from datetime import timedelta
 from dotenv import load_dotenv
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import LLMResult
+from langchain.memory import MomentoChatMessageHistory
+from langchain.schema import HumanMessage, LLMResult, SystemMessage
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from typing import Any
@@ -49,8 +51,25 @@ def handle_mention(event, say):
     thread_ts = event["ts"]
     message = re.sub("<@.*>", "", event["text"])
 
+    # 投稿キー(=Moment キー)：初回=event["ts"], 2回目以降 =event["thread_ts"]
+    id_ts = event["ts"]
+    if "thread_ts" in event:
+        id_ts = event["thread_ts"]
+
     result = say("\n\nTyping...", thread_ts=thread_ts)
     ts = result["ts"]
+
+    history = MomentoChatMessageHistory.from_client_params(
+        id_ts,
+        os.environ["MOMENTO_CACHE"],
+        timedelta(hours=int(os.environ["MOMENTO_TTL"]))
+    )
+
+    messages = [SystemMessage(content="You are a good assistant.")]
+    messages.extend(history.messages)
+    messages.append(HumanMessage(content=message))
+
+    history.add_user_message(message)
 
     callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
     llm = ChatOpenAI(
@@ -60,7 +79,8 @@ def handle_mention(event, say):
         callbacks=[callback],
     )
 
-    llm.predict(message)
+    ai_message = llm(messages)
+    history.add_message(ai_message)
 
 
 if __name__ == "__main__":
