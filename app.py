e602ad2e9a857d57ed9a7baf436185db31f03bfa
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-18 23:07:41 +0900

    feat: 複数回応答する問題を解消

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app = App(
 )
 
 
-@app.event("app_mention")
+# @app.event("app_mention")
 def handle_mention(event, say):
     channel = event["channel"]
     thread_ts = event["ts"]
@@ -81,6 +81,13 @@ def handle_mention(event, say):
 
     ai_message = llm(messages)
     history.add_message(ai_message)
+
+
+def just_ack(ack):
+    ack()
+
+
+app.event("app_mention")(ack=just_ack, lazy=[handle_mention])
 
 
 if __name__ == "__main__":
