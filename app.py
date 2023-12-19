3f97751830384ce0a37975421cf62c5b5f9e8be6
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-19 22:25:39 +0900

    feat: slackメッセージのリッチ化

--- app.py
+++ app.py
@@ -42,7 +42,21 @@ class SlackStreamingCallbackHandler(BaseCallbackHandler):
                 self.interval = self.interval * 2
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-        app.client.chat_update(channel=self.channel, ts=self.ts, text=self.message)
+        message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        message_blocks = [
+            {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
+            {"type": "divider"},
+            {
+                "type": "context",
+                "elements": [{"type": "mrkdwn", "text": message_context}],
+            }
+        ]
+        app.client.chat_update(
+            channel=self.channel,
+            ts=self.ts,
+            text=self.message,
+            blocks=message_blocks,
+        )
 
 
 load_dotenv()
